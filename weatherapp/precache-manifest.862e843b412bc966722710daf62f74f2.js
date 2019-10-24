b87c59c471f997cf4bc23c78ecec0a7457b96881
Amalia Plana <amalia@penni.io>
2019-10-24 09:47:51 +0200

    auto message

---
+++ weatherapp/precache-manifest.862e843b412bc966722710daf62f74f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bceb8bc3dead4e00c878b76fc54052",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "1f0c7780f26b8c20177f",
+    "url": "/myapp/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "d233698653f5134eafff",
+    "url": "/myapp/static/css/main.8f992f31.chunk.css"
+  },
+  {
+    "revision": "1f0c7780f26b8c20177f",
+    "url": "/myapp/static/js/2.10421398.chunk.js"
+  },
+  {
+    "revision": "d233698653f5134eafff",
+    "url": "/myapp/static/js/main.1e6f72d2.chunk.js"
+  },
+  {
+    "revision": "c0f45250989bf9579ca6",
+    "url": "/myapp/static/js/runtime-main.18b17f5d.js"
+  }
+]);
