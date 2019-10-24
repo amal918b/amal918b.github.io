5712dc4cfc7c4e60347b0cedbdc1214d9e332440
Amalia Plana <amalia@penni.io>
2019-10-24 09:49:44 +0200

    auto message

---
+++ weatherapp/precache-manifest.04b4b0caad38922c7761ff1af551a171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb123c8531fab824193e8d7a1bbbc336",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "1f0c7780f26b8c20177f",
+    "url": "/weatherapp/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "eb835190283645ccb320",
+    "url": "/weatherapp/static/css/main.8f992f31.chunk.css"
+  },
+  {
+    "revision": "1f0c7780f26b8c20177f",
+    "url": "/weatherapp/static/js/2.10421398.chunk.js"
+  },
+  {
+    "revision": "eb835190283645ccb320",
+    "url": "/weatherapp/static/js/main.b94bfcf2.chunk.js"
+  },
+  {
+    "revision": "3d7984604ef33e79c26f",
+    "url": "/weatherapp/static/js/runtime-main.fed0f71d.js"
+  }
+]);
--- weatherapp/service-worker.js
+++ weatherapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.862e843b412bc966722710daf62f74f2.js"
+  "/weatherapp/precache-manifest.04b4b0caad38922c7761ff1af551a171.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weatherapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
